b193b5c029b39f2e0d9c407bf5646192225ece65
VantusMantus <vantusmantus@gmail.com>
2019-08-12 16:12:00 +0500

    Add laser explosion
    - При попадании в корабль игрока, появляется эффект вспышки

---
+++ SpaceINK/Assets/Scripts/PlayerShip/LaserBlink.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBlink : MonoBehaviour
+{
+    public GameObject laserParticle;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "EnemyBullet")
+        {
+            GameObject splash = Instantiate(laserParticle, collision.transform.position, transform.rotation);
+            splash.transform.parent = this.transform;
+            Destroy(splash, 2.0f);
+        }
+    }
+
+}
