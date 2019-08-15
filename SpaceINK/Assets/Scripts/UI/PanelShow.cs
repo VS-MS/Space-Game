02ec4ee380d722b7fe7695200f6633a56e52e194
VantusMantus <vantusmantus@gmail.com>
2019-08-15 13:38:44 +0500

    Add main menu animation
    Добавлена анимация для главного меню плавный выезд для панелей Upgrade и Select.

---
+++ SpaceINK/Assets/Scripts/UI/PanelShow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelShow : MonoBehaviour
+{
+    public GameObject upgradeAnim;
+    public GameObject selectAnim;
+
+    public void ShowUpgrade()
+    {
+        upgradeAnim.GetComponent<Animator>().SetBool("Show", true);
+        selectAnim.GetComponent<Animator>().SetBool("Show", false);
+    }
+    public void ShowSelect()
+    {
+        upgradeAnim.GetComponent<Animator>().SetBool("Show", false);
+        selectAnim.GetComponent<Animator>().SetBool("Show", true);
+    }
+}
