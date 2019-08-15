b9537ff0bb233e0e01aa37995c61998714a510a0
VantusMantus <vantusmantus@gmail.com>
2019-08-15 14:08:39 +0500

    Fix main menu animation bug on start
    Исправлен баг анимации, при первом старте панель выезжала частично на экран.

--- SpaceINK/Assets/Scripts/UI/PanelShow.cs
+++ SpaceINK/Assets/Scripts/UI/PanelShow.cs
@@ -7,11 +7,14 @@ public class PanelShow : MonoBehaviour
     public GameObject upgradeAnim;
     public GameObject selectAnim;
 
+    //включить анимацию выезда для панели Upgrade, а для осталных анимацию заезда за экран
     public void ShowUpgrade()
     {
         upgradeAnim.GetComponent<Animator>().SetBool("Show", true);
         selectAnim.GetComponent<Animator>().SetBool("Show", false);
     }
+
+    //включить анимацию выезда для панели Select, а для осталных анимацию заезда за экран
     public void ShowSelect()
     {
         upgradeAnim.GetComponent<Animator>().SetBool("Show", false);
