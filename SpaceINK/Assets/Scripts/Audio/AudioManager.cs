1c0c8b5e817388efa3cf0c2037358ba0070387dd
Dmitry Vlasov <vantusmantus@gmail.com>
2019-09-24 13:41:22 +0500

    Add audioManger
    Добавлен просто AudioManager, позже нужно будет дописать.

---
+++ SpaceINK/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    /*
+     * Для запуска из любого скрипта, можно использовать такую конструкцию
+     * FindObjectOfType<AudioManager>().Play("NazvanieSound");
+     */
+
+    public Sound[] sounds;
+    public static AudioManager instance;
+    void Awake()
+    {
+        
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.Log("sound not found");
+            return;
+        }
+        s.source.Play();
+    }
+}
