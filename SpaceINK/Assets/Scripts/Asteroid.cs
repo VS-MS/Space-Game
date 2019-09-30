bb81324c48f9a5f36dda501a4b988079a3af44c2
Dmitry Vlasov <vantusmantus@gmail.com>
2019-09-30 15:02:04 +0500

    Add asteroid destroy system
    - После уничтожения астероида, он разваливается на мелкте астероиды.

--- SpaceINK/Assets/Scripts/Asteroid.cs
+++ SpaceINK/Assets/Scripts/Asteroid.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    public int fragmentCount = 1; 
+    public Asteroid fragmentAsteroid;
+
+    private int countBullet = 0;
+
     public float torque;
     public float hp;
     private Rigidbody2D rb;
@@ -24,6 +29,21 @@ public class Asteroid : MonoBehaviour
         }
         if(hp <= damage)
         {
+            countBullet ++;
+            //Проверка на количество пуль, вызвавших ReciveDamage
+            //исключаем повторный вызов создания астероидов
+            if(countBullet <= 1 & fragmentCount != 0)
+            {
+                FindObjectOfType<AudioManager>().Play("Explosion5"); //Потом поменять на звук в эффекте взрыва
+                for (int i = 0; i < fragmentCount; i++)
+                {
+                    Vector3 randomPosition = new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f), 0);
+                    Asteroid newAsteroid = Instantiate(fragmentAsteroid, this.transform.position + randomPosition, Quaternion.Euler(0, 0, Random.Range(0, 360))) as Asteroid;
+                    newAsteroid.GetComponent<Rigidbody2D>().AddForce(randomPosition * newAsteroid.torque * 100);
+                }
+                
+            }
+           
             Destroy(gameObject, 0.0f);
         }
     }
