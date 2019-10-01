54f7e508d7d14bf314dc6df1ce29567337f9fbaa
Dmitry Vlasov <vantusmantus@gmail.com>
2019-10-01 16:25:23 +0500

    Add new Target Indicator
    Добавлен новый способ отображения стрелки с направлением цели, которая находиться за экраном.

---
+++ SpaceINK/Assets/Resources/UI/TargetIndicator.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    [SerializeField] private Camera uiCamera;
+
+    private Vector3 targetPosition;
+    //private RectTransform pointerRectTransform;
+    public GameObject targetImage;
+    private GameObject target;
+
+    private void Awake()
+    {
+        targetPosition = new Vector3(-50, 0);
+        target = Instantiate(targetImage, transform.position, transform.rotation);
+        //Debug.Log(transform.Find("Pointer"));
+        //pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
+
+    }
+    private void Update()
+    {
+        Vector3 toPosition = targetPosition;
+        Vector3 fromPosition = Camera.main.transform.position;
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = GetAngleFromVectorFloat(dir);
+        target.transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        float borderSize = 40f;
+
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(targetPosition);
+        bool isOffscreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
+        Debug.Log(isOffscreen + " " + targetPositionScreenPoint);
+
+        if (isOffscreen)
+        {
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            cappedTargetScreenPosition.x = Mathf.Clamp(cappedTargetScreenPosition.x, borderSize, Screen.width - borderSize);
+            cappedTargetScreenPosition.y = Mathf.Clamp(cappedTargetScreenPosition.y, borderSize, Screen.height - borderSize);
+
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(cappedTargetScreenPosition);
+            target.transform.position = pointerWorldPosition;
+            target.transform.localPosition = new Vector3(target.transform.localPosition.x, target.transform.localPosition.y, 0f);
+
+        }
+        else
+        {
+            Vector3 pointerWorldPosition = uiCamera.ScreenToWorldPoint(targetPositionScreenPoint);
+            target.transform.position = pointerWorldPosition;
+            target.transform.localPosition = new Vector3(target.transform.localPosition.x, target.transform.localPosition.y, 0f);
+
+        }
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+}
--- SpaceINK/Assets/Scripts/UI/TargetPosition.cs
+++ SpaceINK/Assets/Scripts/UI/TargetPosition.cs
@@ -15,12 +15,54 @@ public class TargetPosition : MonoBehaviour
         colorArrow = targetArrow.GetComponent<SpriteRenderer>().color;
     }
 
-    private void Start()
+    public static float GetAngleFromVectorFloat(Vector3 dir)
     {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
 
+        return n;
+    }
+    private void FollowTarget_()
+    {
+        ColorDistance();
+        SizeDistance();
+
+        Vector3 toPosition = target.transform.position;
+        Vector3 fromPosition = Camera.main.transform.position;
+        fromPosition.z = 0f;
+        Vector3 dir = (toPosition - fromPosition).normalized;
+        float angle = GetAngleFromVectorFloat(dir);
+        targetArrow.transform.localEulerAngles = new Vector3(0, 0, angle);
+
+        float borderSize = 40f;
+
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(target.transform.position);
+        bool isOffscreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
+        //Debug.Log(isOffscreen + " " + targetPositionScreenPoint);
+
+        if (isOffscreen)
+        {
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+            cappedTargetScreenPosition.x = Mathf.Clamp(cappedTargetScreenPosition.x, borderSize, Screen.width - borderSize);
+            cappedTargetScreenPosition.y = Mathf.Clamp(cappedTargetScreenPosition.y, borderSize, Screen.height - borderSize);
+
+            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(cappedTargetScreenPosition);
+            targetArrow.transform.position = pointerWorldPosition;
+            targetArrow.transform.localPosition = new Vector3(targetArrow.transform.localPosition.x, targetArrow.transform.localPosition.y, 0f);
+
+        }
+        else
+        {
+            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(targetPositionScreenPoint);
+            targetArrow.transform.position = pointerWorldPosition;
+            targetArrow.transform.localPosition = new Vector3(targetArrow.transform.localPosition.x, targetArrow.transform.localPosition.y, 0f);
+
+        }
     }
     private void FollowTarget()
     {
+
         if (target)
         {
             if (target.tag == "EnemyShip")
@@ -76,10 +118,20 @@ public class TargetPosition : MonoBehaviour
     {
         var heading = targetArrow.transform.position - target.transform.position;
         var distance = heading.magnitude;
+        
         if (distance < 2)
         {
-            colorArrow.a = distance/2;
-            targetArrow.GetComponent<SpriteRenderer>().color = colorArrow;
+            if(distance < 0.1)
+            {
+                colorArrow.a = 0f;
+                targetArrow.GetComponent<SpriteRenderer>().color = colorArrow;
+            }
+            else
+            {
+                colorArrow.a = distance / 2;
+                targetArrow.GetComponent<SpriteRenderer>().color = colorArrow;
+            }
+            
         }
         else
         {
@@ -91,7 +143,29 @@ public class TargetPosition : MonoBehaviour
 
     void Update()
     {
-        FollowTarget();
-        //ColorDistance();
+        if(target)
+        {
+            if (target.tag == "EnemyShip")
+            {
+                if (target.GetComponent<Unit>().shipState != Unit.State.Die)
+                {
+                    FollowTarget_();
+                }
+                else
+                {
+                    Destroy(targetArrow);
+                    Destroy(this);
+                }
+            }
+            else
+            {
+                FollowTarget_();
+            }
+        }
+        else
+        {
+            Destroy(targetArrow);
+            Destroy(this);
+        }
     }
 }
