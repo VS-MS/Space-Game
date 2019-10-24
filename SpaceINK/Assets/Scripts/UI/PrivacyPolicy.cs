8db1dab10d67203ad1869fb284f6da01b0b0d138
Dmitry Vlasov <vantusmantus@gmail.com>
2019-10-24 16:25:15 +0500

    Add ads icon. Add PrivacyPolicy URL

---
+++ SpaceINK/Assets/Scripts/UI/PrivacyPolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyPolicy : MonoBehaviour
+{
+    public string url_;
+    public void OpenPrivacyPolicy()
+    {
+        Application.OpenURL(url_);
+    }
+}
