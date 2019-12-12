d8dde735a184bc120cb5910b0d86d7b4331abc62
Dmitry Vlasov <vantusmantus@gmail.com>
2019-12-12 10:43:26 +0500

    Fix last lvl in game
    Исправлена ошибка, которая вызывала бесконечную загрузку после прохождения последнего уровня.

---
+++ SpaceINK/Assets/Scripts/UI/LastLvl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class LastLvl : MonoBehaviour
+{
+    public int maxLvl;
+    private Button nextLvl;
+    private void Awake()
+    {
+        string s = SceneManager.GetActiveScene().name;
+        int lvlNumber;
+        try
+        {
+            lvlNumber = Convert.ToInt32(s);
+        }
+        catch (System.FormatException)
+        {
+            lvlNumber = 1;
+            Debug.LogError("Не верное название сцены, сцена должна называться только целочисленным числом. Установленно значение по умолчанию равное = " + lvlNumber);
+        }
+        Debug.Log(lvlNumber);
+        Debug.Log(maxLvl);
+
+        nextLvl = GetComponent<Button>();
+        if(maxLvl >= lvlNumber)
+        {
+            nextLvl.interactable = false;
+        }
+        else
+        {
+            nextLvl.interactable = true;
+        }
+
+    }
+}
